b992d90835236b910c5af406ac50ec609141497f
Henry Hsieh <r901042004@yahoo.com.tw>
2024-05-30 20:15:14 +0800

    chore: update AppRun script

--- data/htop.bash
+++ data/htop.bash
@@ -3,4 +3,4 @@
 APPIMAGE_DIR=$(dirname $(realpath $0))
 TERMINFO_DIRS=${APPIMAGE_DIR}/usr/share/terminfo${TERMINFO_DIRS:+:$TERMINFO_DIRS} \
 LD_LIBRARY_PATH=${APPIMAGE_DIR}/usr/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH} \
-${APPIMAGE_DIR}/usr/bin/htop $@
+${APPIMAGE_DIR}/usr/bin/htop "$@"
